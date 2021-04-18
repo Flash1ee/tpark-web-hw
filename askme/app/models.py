28bdd199bb7dee76ac6eec92ac7102fff1541c02
Dmitry Varin <varindv@gmail.com>
2021-04-18 16:10:37 +0300

    add models for project

--- askme/app/models.py
+++ askme/app/models.py
@@ -1,3 +1,68 @@
+import django.contrib.auth.models
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    birth_date = models.DateField()
+    avatar = models.ImageField(default=None)
+
+    def __str__(self):
+        return self.user.username
+    class Meta:
+        verbose_name = "Автор"
+        verbose_name_plural = "Авторы"
+
+class Tag(models.Model):
+    name = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.name
+    class Tag:
+        verbose_name = "Тег"
+        verbose_name_plural = 'Теги'
+
+
+
+class Like(models.Model):
+    count = models.IntegerField(8)
+
+    class Meta:
+        verbose_name = 'Лайк'
+        verbose_name_plural = 'Лайки'
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    pub_date = models.DateTimeField(auto_now=True)
+    likes = models.OneToOneField(Like, on_delete=models.CASCADE)
+
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Вопрос"
+        verbose_name_plural = "Вопросы"
+
+
+class Answer(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    pub_date = models.DateTimeField(auto_now=True)
+    likes = models.OneToOneField(Like, on_delete=models.CASCADE)
+
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Ответ"
+        verbose_name_plural = "Ответы"
+
--- askme/askme/settings.py
+++ askme/askme/settings.py
@@ -35,8 +35,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 ]
-
+MEDIA_ROOT = "media/"
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -74,10 +75,20 @@ WSGI_APPLICATION = 'askme.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'askme',
+        'USER': 'flashie',
+        'PASSWORD': '1111',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
